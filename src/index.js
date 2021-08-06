3f49633120ec892a54e4eec6c3a281ff0e55a118
smita Raj <smita.raj12@gmail.com>
2021-08-06 15:42:22 -0700

    add functionality

--- src/index.js
+++ src/index.js
@@ -12,14 +12,21 @@ function clearFields() {
 }
 
 function getElements(response) {
+  if(response.sys){
+    $('.country').text(`The Country ${response.sys.country}`);
+  }
   if (response.main) {
+    
     $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
     $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    let F = ((9/5*((response.main.temp)-273))) + 32;
+    $('.showTempF').text(`The temperature in Fahrenheit ${F}`);
+    $('.feels-like').text(`The temperature feels like ${response.main.feels_like}`);
   } else {
     $('.showErrors').text(`There was an error: ${response.message}`);
   }
 }
-
+//F = 9/5(K - 273) + 32
 async function makeApiCall(city) {
   const response = await WeatherService.getWeather(city);
   getElements(response);
@@ -30,5 +37,6 @@ $(document).ready(function() {
     let city = $('#location').val();
     clearFields();
     makeApiCall(city);
+    
   });
 });
